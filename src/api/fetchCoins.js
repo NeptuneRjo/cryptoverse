b201785d4414fd0bea41a6acbb3cfd278cf45d3c
NeptuneRjo <rjo9000@outlook.com>
2022-03-25 02:27:57 -0400

    setup fetchCoin api call to be custom hook

--- src/api/fetchCoins.js
+++ src/api/fetchCoins.js
@@ -1,18 +1,57 @@
-const fetchCoins = async () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-            'X-RapidAPI-Key': '8ab56c9046mshf9298833651359cp1b0994jsndcc03fa4cad9'
-        }
-    };
+// const fetchCoins = async () => {
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//             'X-RapidAPI-Key': '8ab56c9046mshf9298833651359cp1b0994jsndcc03fa4cad9'
+//         }
+//     };
 
-    fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            return response;
-        }).catch(err => console.error(err))
+//     fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
+//         .then(response => response.json())
+//         .then(response => {
+//             console.log(response)
+//             return response;
+//         }).catch(err => console.error(err))
+// }
+
+// export default fetchCoins;
+import { useEffect, useState } from 'react';
+
+const FetchCoins = () => {
+  
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController()
+
+        fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setError(err.message);
+                    setIsPending(false);
+                }
+            });
+            
+            return () => abortCont.abort();
+    }, []);
+
+    return { data, isPending, error };
 }
 
-export default fetchCoins;
+export default FetchCoins
