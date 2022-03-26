5eb91ea2653a7dae404ec6deb9b4ecafad0df1bb
NeptuneRjo <rjo9000@outlook.com>
2022-03-26 19:44:09 -0400

    passed coin data as props, rendered

--- src/components/Cryptos/Cryptos.jsx
+++ src/components/Cryptos/Cryptos.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
-const Cryptos = () => {
+const Cryptos = ({ coinData, index }) => {
+
   return (
-    <div>Cryptos</div>
+    <div className="cryptos-main" index={index} >
+      <div className="cryptos-info">
+        <div className="cryptos-info-number">
+          {coinData.rank}.
+        </div>
+        <div className="cryptos-info-name">
+          {coinData.name}
+        </div>
+        <div className="cryptos-info-price">
+          ${Number(coinData.price).toFixed(2)}
+        </div>
+      </div>
+      <div className="cryptos-icon">
+        <img 
+          src={coinData.iconUrl} 
+          alt="Cryptocurrency icon" 
+          className="cryptos-icon-img"
+        />
+      </div>
+    </div>
   )
 }
 
