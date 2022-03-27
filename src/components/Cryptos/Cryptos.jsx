06b186222c1b002446d6abc34d30aa6d97eb6266
NeptuneRjo <rjo9000@outlook.com>
2022-03-27 13:08:39 -0400

    removed jsx

--- src/components/Cryptos/Cryptos.jsx
+++ src/components/Cryptos/Cryptos.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import './style.css';
 
-const Cryptos = ({ coinData, index }) => {
+const Cryptos = () => {
 
   return (
-    <div className="cryptos-main" index={index} >
-      <div className="cryptos-info">
-        <div className="cryptos-info-number">
-          {coinData.rank}.
-        </div>
-        <div className="cryptos-info-name">
-          {coinData.name}
-        </div>
-        <div className="cryptos-info-price">
-          ${Number(coinData.price).toFixed(2)}
-        </div>
-      </div>
-      <div className="cryptos-icon">
-        <img 
-          src={coinData.iconUrl} 
-          alt="Cryptocurrency icon" 
-          className="cryptos-icon-img"
-        />
-      </div>
+    <div className="cryptos-main">
+      cryptos
     </div>
   )
 }
