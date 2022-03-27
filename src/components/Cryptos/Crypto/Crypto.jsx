ddd6a67455409805fc00f30ecb7cda1757c374e9
NeptuneRjo <rjo9000@outlook.com>
2022-03-27 17:25:30 -0400

    crypto components now take you to the coinbase page on click

--- src/components/Cryptos/Crypto/Crypto.jsx
+++ src/components/Cryptos/Crypto/Crypto.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Crypto = ({ coinData, index }) => {
 
   return (
-    <div className="crypto-main" index={index} >
-      <div className="crypto-info">
-        <div className="crypto-info-number">
-          {coinData.rank}.
+    <a href={coinData.coinrankingUrl} >
+      <div className="crypto-main" index={index} >
+        <div className="crypto-info">
+          <div className="crypto-info-number">
+            {coinData.rank}.
+          </div>
+          <div className="crypto-info-name">
+            {coinData.name}
+          </div>
+          <div className="crypto-info-price">
+            ${Number(coinData.price).toFixed(2)}
+          </div>
         </div>
-        <div className="crypto-info-name">
-          {coinData.name}
-        </div>
-        <div className="crypto-info-price">
-          ${Number(coinData.price).toFixed(2)}
+        <div className="crypto-icon">
+          <img 
+            src={coinData.iconUrl} 
+            alt="Cryptocurrency icon" 
+            className="crypto-icon-img"
+          />
         </div>
       </div>
-      <div className="crypto-icon">
-        <img 
-          src={coinData.iconUrl} 
-          alt="Cryptocurrency icon" 
-          className="crypto-icon-img"
-        />
-      </div>
-    </div>
+    </ a>
   )
 }
 
