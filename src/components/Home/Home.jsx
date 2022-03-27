f42de9bab9eed82c56f3665694e5ee91dd76ec46
NeptuneRjo <rjo9000@outlook.com>
2022-03-27 12:35:48 -0400

    aligned to remote

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,54 @@
 import React from 'react'
-import { fetchCoins } from '../../api'
+import { useFetchCoins } from '../../api'
+import './style.css';
 
 const Home = () => {
 
-  // const coinData = fetchCoins()
+  const { data, isPending, error } = useFetchCoins()
 
-  console.log(fetchCoins())
+  let mainStats;
+  let coins;
+  
+  if (!isPending) {
+    mainStats = data.data.stats;
+    coins = data.data.coins.slice(0, 10);
+  }
 
   return (
     <div className="home-main">
-      <div className="home-stats">
-        <div className="home-stats-item" id="total-cryptos"></div>
-        <div className="home-stats-item" id="24hr-volume"></div>
-        <div className="home-stats-item" id="total-exchanges"></div>
+      {isPending &&
+      <div className="home-stats-pending">
+      Loading...
       </div>
+      }
+      {!isPending &&
+        <div className="home-main-loaded">
+          <div className="home-stats">
+            <div className="home-stats-item" id="total-cryptos">
+              {mainStats.total}
+              <p>Total Cryptocurrencies</p>
+            </div>
+            <div className="home-stats-item" id="24hr-volume">
+              {mainStats.total24hVolume}
+              <p>Total 24hr Volume</p>
+            </div>
+            <div className="home-stats-item" id="total-exchanges">
+              {mainStats.totalExchanges}
+              <p>Total Exchanges</p>
+            </div>
+          </div>
+          <div className="home-cryptos">
+            <h3>Top 10 Cryptocurrencies</h3>
+            <div className="home-cryptos-grid">
+              {
+                coins.map(item => {
+                  
+                })
+              }
+            </div>
+          </div>
+        </div>
+      }
     </div>
   )
 }
