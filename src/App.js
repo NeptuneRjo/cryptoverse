ac4a980f16e978331c8dad4ba8fdb70ac7f8f652
NeptuneRjo <rjo9000@outlook.com>
2022-03-27 15:17:43 -0400

    moved fetch calls to app so only one call is necessary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import { Cryptos, Home, Nav, News } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { default as useFetch } from './api/useFetch';
 
 function App() {
 
-  // const coinData = await fetchCoins();
-  // const newsData = await fetchNews();
+  const api = {
+    coinUrl: 'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0',
+    coinHost: 'coinranking1.p.rapidapi.com',
+
+    newsUrl: 'https://crypto-open-news.p.rapidapi.com/news',
+    newsHost: 'crypto-open-news.p.rapidapi.com'
+  }
+
+  const { data: coinData, isPending: coinIsPending, error: coinError } = useFetch(api.coinUrl, api.coinHost);
+  const { data: newsData, isPending: newsIsPending, error: newsError } = useFetch(api.newsUrl, api.newsHost);
+
+  const coinProps = {
+    data: coinData,
+    isPending: coinIsPending,
+    error: coinError
+  }
+
+  const newsProps = {
+    data: newsData,
+    isPending: newsIsPending,
+    error: newsError
+  }
 
   return (
     <BrowserRouter>
@@ -17,6 +38,8 @@ function App() {
               exact path='/' 
               element={
               <Home 
+                coinProps={coinProps}
+                newsProps={newsProps}
               />
             } 
             />
