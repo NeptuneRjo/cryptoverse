13b37616787b905b84b7631a9aa4a8d7205de7ac
NeptuneRjo <rjo9000@outlook.com>
2022-03-28 17:07:21 -0400

    added render conditions for news

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.css';
 import Crypto from '../Cryptos/Crypto/Crypto';
 import store from '../../store';
+import NewsItem from '../News/NewsItem/NewsItem';
 
 const Home = ({ coinProps, newsProps }) => {
 
@@ -12,22 +13,30 @@ const Home = ({ coinProps, newsProps }) => {
   const coinData = coinProps.data;
   const coinPending = coinProps.isPending;
 
+  const newsData = newsProps.data
+  const newsPending = newsProps.isPending;
+
   let mainStats;
   let coins;
+  let news;
   
   if (!coinPending) {
     mainStats = coinData.data.stats;
     coins = coinData.data.coins.slice(0, 10);
   }
 
+  if (!newsPending) {
+    news = newsData.news[0];
+  }
+
   return (
     <div className="home-main">
-      {coinPending &&
+      {(coinPending || newsPending) &&
       <div className="home-stats-pending">
       Loading...
       </div>
       }
-      {!coinPending &&
+      {(!coinPending && !newsPending) &&
         <div className="home-main-loaded">
           <div className="home-stats">
             <div className="home-stats-item" id="total-cryptos">
@@ -54,6 +63,12 @@ const Home = ({ coinProps, newsProps }) => {
                   />
                 ))
               }
+            </div>
+          </div>
+          <div className="home-news">
+            <h3>Latest News</h3>
+            <div className="home-news-grid">
+              <NewsItem news={news} />
             </div>
           </div>
         </div>
