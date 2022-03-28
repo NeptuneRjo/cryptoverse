aeea46071734d17beb7bae24bb571b210a41831d
NeptuneRjo <rjo9000@outlook.com>
2022-03-28 17:06:37 -0400

    added jsx; created function to add https to image urls for those that dont have one

---
+++ src/components/News/NewsItem/NewsItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../style.css';
+
+const NewsItem = ({ news }) => {
+
+    const image = news.imageUrl;
+
+    const checkImageUrl = (url) => {
+        const urlString = url.toLowerCase();
+        const https = 'https://'
+
+        if (!urlString.includes('http')) {
+            return  (
+                https.concat(url)
+            )
+        }
+        return url
+    }
+
+    return (
+        <a href={news.articleUrl}>
+            <div className="newsitem-main">
+                <div className="newsitem-hero">
+                   <img src={checkImageUrl(image)} alt="" />
+                </div>
+                <div className="newsitem-info">
+                    <div className="newsitem-header">
+                        {news.title}
+                    </div>
+                    <div className="newsitem-date">
+                        {news.datestamp}
+                    </div>
+                    <div className="newsitem-tags">
+                        {
+                            news.categories.map((tag, index) => (
+                                <p key={index}>{tag}</p>
+                            ))
+                        }
+                    </div>
+                    <div className="newsitem-click">click to view more...</div>
+                    <div className="newsitem-tap">tap to view more...</div>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default NewsItem
