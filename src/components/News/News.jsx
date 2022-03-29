95ba6beb3edf1d340e67340b9f939f5afff81231
NeptuneRjo <rjo9000@outlook.com>
2022-03-28 23:59:53 -0400

    built news component

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,14 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import store from '../../store'
+import NewsItem from './NewsItem/NewsItem'
 
-const News = () => {
+const News = ({ newsProps }) => {
+
+  const newsPending = newsProps.isPending;
+
+  const [q, setQ] = useState('')
+  const [searchParam] = useState(['categories'])
+
+  let articles;
+
+  if (!newsPending) {
+    articles = newsProps.data.news
+    console.log(articles);
+  }
+
+  const search = (items) => {
+    return items.filter((item) => {
+      return searchParam.some((newItem) => {
+        return (
+          item[newItem]
+            .toString()
+            .toLowerCase()
+            .indexOf(q.toLocaleLowerCase()) > -1
+        )
+      });
+    });
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
   
-  store.dispatch(
-    { type: 'SET_NAVBAR', payload: 'news' }
-  )
+  useEffect(() => {
+    store.dispatch(
+      { type: 'SET_NAVBAR', payload: 'news' }
+    )
+  }, [])
+  
 
+  if (newsPending) {
+    return (
+      <div className="news-pending">Loading...</div>
+    )
+  }
   return (
-    <div>News</div>
+    <div className="news-main">
+      <div className="news-header">
+        <h3>Today's Crypto News</h3>
+      </div>
+      <div className="news-search">
+        <label htmlFor="search-form">
+          <input 
+            type="search" 
+            name="search-form" 
+            id="news-search-form" 
+            placeholder='Search for a tag'
+            value={q}
+            onChange={(e) => setQ(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="news-grid">
+        {
+          search(articles).map((news, index) => (
+            <NewsItem
+              news={news}
+              key={index}
+            />
+          ))
+        }
+      </div>
+      <div className="news-totop">
+        <span onClick={scrollToTop}>Back to top</span>
+      </div>
+    </div>
   )
 }
 
