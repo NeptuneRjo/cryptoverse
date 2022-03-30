a29d72a605564e77fc6f93333efecba34975bad4
NeptuneRjo <rjo9000@outlook.com>
2022-03-30 01:27:39 -0400

    added new jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -23,43 +23,106 @@ const Nav = () => {
     )
   }
 
-  return (
-    <div className="nav-main">
-      <div className="nav-desktop">
+  function disableScroll(){
+    document.getElementById('scrollable').classList.add('disable-scroll');
+  }
+  
+  function enableScroll(){
+    document.getElementById('scrollable').classList.remove('disable-scroll');
+  }
 
-      </div>
-      <div className="nav-mobile">
-        {toggleMenu
-          ? <RiCloseLine color='#393E46' size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color='#FFD369' size={27} onClick={() =>setToggleMenu(true)} /> 
-        }
+  return (
+    <>
+      <div className={`nav-mobile-main ${toggleMenu}`}>
+        <div className="nav-mobile-toggle">
+          {toggleMenu
+            ? <RiCloseLine color='#393e46' size={27} onClick={() => {setToggleMenu(false)}} />
+            : <RiMenu3Line color='#ffd369' size={27} onClick={() => {setToggleMenu(true)}} />
+          }
+        </div>
         {toggleMenu && (
-          <div className="nav-mobile-links">
-            <Link 
-              className={`nav-mobile-links-item ${navbar.currentNav === 'home'}`} 
-              to='/' 
-              onClick={() => updateNavState('home')}
-              >
-              Home
-            </Link>
-            <Link 
-              className={`nav-mobile-links-item ${navbar.currentNav === 'cryptos'}`} 
-              to='/crypto'
-              onClick={() => updateNavState('cryptos')}
-              >
-              Cryptocurrencies
-            </Link>
-            <Link 
-              className={`nav-mobile-links-item ${navbar.currentNav === 'news'}`} 
-              to='/news'
-              onClick={() => updateNavState('news')}
-              >
-              News
-            </Link>
-          </div>
+          <div className="nav-mobile-menu">
+              <div className="nav-mobile-menu-item">
+                <Link
+                  className={
+                    `nav-mobile-menu-link 
+                    ${navbar.currentNav === 'home'}`
+                  }
+                  to='/'
+                  onClick={() => updateNavState('home')}
+                >
+                  <div>Home</div>
+                </Link>
+              </div>
+              <div className="nav-mobile-menu-item">
+                <Link
+                  className={
+                    `nav-mobile-menu-link 
+                    ${navbar.currentNav === 'cryptos'}`
+                  }
+                  to='/crypto'
+                  onClick={() => updateNavState('cryptos')}
+                >
+                  <div>Cryptos</div>
+                </Link>
+              </div>
+              <div className="nav-mobile-menu-item">
+                <Link
+                  className={
+                    `nav-mobile-menu-link 
+                    ${navbar.currentNav === 'news'}`
+                  }
+                  to='/news'
+                  onClick={() => updateNavState('news')}
+                >
+                  <div>News</div>
+                </Link>
+              </div>
+          </div>  
         )}
       </div>
-    </div>
+      <div className="nav-desktop-main">
+
+      </div>
+    </>
+
+    // <div className={`nav-main ${toggleMenu}`}>
+    //   <div className="nav-desktop">
+
+    //   </div>
+    //   <div className={`nav-mobile ${toggleMenu}`}>
+    //     {toggleMenu
+    //       ? <RiCloseLine color='#393E46' size={27} onClick={() => {setToggleMenu(false); disableScroll()}} />
+    //       : <RiMenu3Line color='#FFD369' size={27} onClick={() => {setToggleMenu(true); enableScroll()}} /> 
+    //     }
+    //     {toggleMenu && (
+    //       <div className="nav-mobile-links">
+    //         <Link 
+    //           className={`nav-mobile-links-item ${navbar.currentNav === 'home'}`} 
+    //           to='/' 
+    //           onClick={() => updateNavState('home')}
+    //           >
+    //           <div>Home</div>
+    //           {/* Home */}
+    //         </Link>
+    //         <Link 
+    //           className={`nav-mobile-links-item ${navbar.currentNav === 'cryptos'}`} 
+    //           to='/crypto'
+    //           onClick={() => updateNavState('cryptos')}
+    //           >
+    //           <div>Cryptocurrencies</div>
+    //         </Link>
+    //         <Link 
+    //           className={`nav-mobile-links-item ${navbar.currentNav === 'news'}`} 
+    //           to='/news'
+    //           onClick={() => updateNavState('news')}
+    //           >
+    //           <div>News</div>
+    //         </Link>
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
   )
 }
 
