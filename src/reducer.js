5d32d0192a191431cc1d34ce746a1a5d632b751f
NeptuneRjo <rjo9000@outlook.com>
2022-04-01 16:39:06 -0400

    added api reducers to rootReducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,18 @@ import { combineReducers } from 'redux';
 
 import navReducer from './components/Nav/navReducer';
 
+import coinReducer from './api/reducers/Coins/coinReducer';
+import coinDetailsReducer from './api/reducers/Coins/coinDetailsReducer';
+
+import newsReducer from './api/reducers/News/newsReducer';
+import newsDetailsReducer from './api/reducers/News/newsDetailsReducer';
+
 const rootReducer = combineReducers({
     navbar: navReducer,
+    coinApi: coinReducer,
+    coinDetailsApi: coinDetailsReducer,
+    newsApi: newsReducer,
+    newsDetailsApi: newsDetailsReducer,
 })
 
 export default rootReducer;
