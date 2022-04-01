16297ea1b0480cbde04d4ccdb62fd896459936fd
NeptuneRjo <rjo9000@outlook.com>
2022-04-01 18:46:37 -0400

    added redux state reference for the article index

--- src/components/News/NewsPage/NewsPage.jsx
+++ src/components/News/NewsPage/NewsPage.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import './style.css';
-import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Spinner from '../../../animations/Spinner/Spinner';
 
 const NewsPage = () => {
-    const { newsId } = useParams();
+  const parse = require('html-react-parser');
 
+  const newsApi = useSelector((state) => state.newsApi)
+  const newsIndex = useSelector((state) => state.newsIndex)
+
+  let article;
+
+  if (!newsApi.isPending) {
+    console.log(newsApi.data.news[newsIndex.index])
+
+    article = newsApi.data.news[newsIndex.index];
+  }
+
+  const checkImageUrl = (url) => {
+    const urlString = url.toLowerCase();
+    const https = 'https://'
+
+    if (!urlString.includes('http')) {
+        return  (
+            https.concat(url)
+        )
+    }
+    return url
+  }
+
+  if (newsApi.isPending) {
+    return (
+      <div className="newspage-loading">
+        <Spinner />
+      </div>
+    )
+  }
   return (
-    <div>NewsPage</div>
+    <div className="newspage-main">
+      <div className="newspage-header">
+        <h3>{article.title}</h3>
+      </div>
+      <div className="newspage-hero">
+        <img src={checkImageUrl(article.imageUrl)} />
+      </div>
+      <div className="newspage-author">
+        <a href={article.articleUrl}>Author</a>
+      </div>
+    </div>
   )
 }
 
