b7fcff6bee2c8332f2020b035d73ecee2b353fd7
NeptuneRjo <rjo9000@outlook.com>
2022-04-03 13:40:48 -0400

    completed navbar tests

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -6,47 +6,60 @@ import { createTestStore } from './createTestStore';
 
 import App from '../App';
 import { Nav, Cryptos, Home, News } from '../components';
-
+import { store as actualStore } from '../store';
+import { useSelector } from 'react-redux';
 
 let store;
 
 describe('App', () => {
-    let div = document.createElement('div')
 
     beforeEach(() => {
         store = createTestStore();
     });
     
-    const appComponent = () => {
+    const component = (component) => {
         render(
         <Provider store={store} >
-            <Nav />
-        </Provider>
+            {component}
+        </Provider>,
+        {wrapper: MemoryRouter}
     )}
 
-    const navComponent = () => {
-        render(
-            <Provider store={store} >
-                <Nav />
-            </Provider>, 
-            {wrapper: MemoryRouter}
-        )
-    }
+    describe('Nav', () => {
 
-    it('should render the desktop nav', async () => {
-        navComponent();
+        it('should render the desktop nav', async () => {
+            component(<Nav />)
+    
+            const home = screen.getByTestId('home-link-desktop')
+            const cryptos = screen.getByTestId('crypto-link-desktop')
+            const news = screen.getByTestId('news-link-desktop');
+    
+            expect(home).toBeVisible;
+            expect(cryptos).toBeVisible;
+            expect(news).toBeVisible;
+        })
 
-        const home = screen.getByTestId('home-link-desktop')
-        const cryptos = screen.getByTestId('crypto-link-desktop')
-        const news = screen.getByTestId('news-link-desktop');
+        it('should render the mobile nav', async () => {
+            component(<Nav />)
+            
+            const home = screen.getByTestId('home-link-mobile')
+            const cryptos = screen.getByTestId('crypto-link-mobile')
+            const news = screen.getByTestId('news-link-mobile');
 
-        expect(home).toBeVisible;
-        expect(cryptos).toBeVisible;
-        expect(news).toBeVisible;
+            expect(home).toBeVisible;
+            expect(cryptos).toBeVisible;
+            expect(news).toBeVisible;
+        })
+
+        it('should switch the nav toggle when clicked', () => {
+            component(<Nav />)
+
+            const openMenu = screen.getByTestId('3-line-menu')
+            expect(openMenu).toBeVisible
+
+            fireEvent.click(openMenu)
+            const closeMenu = screen.getByTestId('close-menu')
+            expect(closeMenu).toBeVisible
+        })
     })
-    // describe('Nav', () => {
-    //     it('should render the desktop nav', () => {
-
-    //     })
-    // })
 })
