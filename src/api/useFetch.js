e3a0a797364107a8e04669d3eb9874aa7feead93
NeptuneRjo <rjo9000@outlook.com>
2022-04-25 16:12:21 -0400

    cleanup

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import store from '../store';
 
 const useFetch = (url, host, type) => {
@@ -11,11 +11,6 @@ const useFetch = (url, host, type) => {
         }
     };
 
-    // store.dispatch({type: `${type}_FETCH_PENDING`});
-  
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         const abortCont = new AbortController()
@@ -28,74 +23,24 @@ const useFetch = (url, host, type) => {
                 return res.json();
             })
             .then(data => {
-                // setData(data);
-
                 store.dispatch(
                     {type:`${type}_FETCH_COMPLETED`, payload: data}
                 );
-
-                // setIsPending(false);
-                // setError(null);
             })
             .catch(err => {
                 if (err.name === 'AbortError') {
                     console.log('fetch aborted')
                 } else {
                     store.dispatch(
-                        {type: `${type}_FETCH_FAILED`, payload: error}
+                        {type: `${type}_FETCH_FAILED`, payload: err}
                     );
-
-                    // setError(err.message);
-                    // setIsPending(false);
                 }
             });
             
             return () => abortCont.abort();
     }, []);
 
-    // return { data, isPending, error };
     return;
 }
 
 export default useFetch
-
-// import { 
-//     fetchApisBegin, 
-//     fetchApisSuccess, 
-//     fetchApisFailure
-// } from "./apiActions";
-// import { useEffect, useState } from "react";
-
-// function useFetch(url, host) {
-
-//     const [data, setData] = useState(null);
-//     const [isPending, setIsPending] = useState(true);
-//     const [error, setError] = useState(null);
-
-
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Host': host,
-//             'X-RapidAPI-Key': process.env.REACT_APP_X_RPD_KY
-//         }
-//     };
-
-//     return dispatch => {
-//         dispatch(fetchApisBegin());
-//         fetch(url, options)
-//         .then(res => res.json())
-//         .then(res => {
-//             if(res.error) {
-//                 throw(res.error);
-//             }
-//             dispatch(fetchApisSuccess(res.data))
-//             return res.data
-//         })
-//         .catch(error => {
-//             dispatch(fetchApisFailure(error))
-//         })
-//     }
-// }
-
-// export default fetchProducts
