614e063559463406f15075770c12ae9ec0e7f9c0
NeptuneRjo <rjo9000@outlook.com>
2022-04-29 20:59:33 -0400

    crypto component now uses MUI

--- src/components/Cryptos/Crypto/Crypto.jsx
+++ src/components/Cryptos/Crypto/Crypto.jsx
@@ -1,35 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material'
+import { styled } from '@mui/material/styles';
 
 const Crypto = ({ coinData }) => {
+
+  const CustomizedCard = styled(Card)`
+    color: #fff;
+    background: #303030;
+
+  `;
+
+  const CustomizedCardContent = styled(CardContent)`
+    background: #3f51b5;
+  `;
 
   return (
     <Link to={`/crypto/${coinData.uuid}`}               data-testid='crypto-item'
     >
-      <div className="crypto-main" >
-        <div className="crypto-info">
-          <div className="crypto-info-number" data-testid='crypto-num'>
-            {coinData.rank}.
-          </div>
-          <div className="crypto-info-name" data-testid='crypto-name'>
-            {coinData.name}
-          </div>
-          <div className="crypto-info-price" data-testid='crypto-price'>
-            ${Number(coinData.price).toFixed(2)}
-          </div>
-          <p className='crypto-tap'>tap to see more</p>
-          <p className='crypto-click'>click to see more</p>
-        </div>
-        <div className="crypto-icon" >
-          <img 
-            data-testid='crypto-icon'
-            src={coinData.iconUrl} 
-            alt="Cryptocurrency icon" 
-            className="crypto-icon-img"
+      <CustomizedCard width={100}>
+        <CardActionArea
+          sx={{
+            display: 'flex',
+            flexDirect: 'row-reverse',
+            justifyContent: 'space-between',
+            background: 'transparent'
+          }}
+        >
+          <CardContent
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '50%',
+              background: 'transparent'
+            }}
+          >
+            <CardMedia 
+              component='img'
+              image={coinData.iconUrl}
+              alt='coin icon'
+              sx={{
+                height: '35px',
+                width: '35px',
+                background: 'transparent'
+              }}
           />
-        </div>
-      </div>
+          </CardContent>
+          <CustomizedCardContent 
+            sx={{
+              width: '50%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRight: 'solid 2px #303030'
+            }}
+          >
+            <Typography 
+              variant='h6' 
+              component='div' 
+              backgroundColor='transparent'
+            >
+              {coinData.name}
+            </Typography>
+          </CustomizedCardContent>
+        </CardActionArea>
+        </CustomizedCard>
     </ Link>
   )
 }
