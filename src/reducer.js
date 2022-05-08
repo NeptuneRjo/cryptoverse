4a3f0eaf252f542896a91a15e5b3548a0f055357
NeptuneRjo <rjo9000@outlook.com>
2022-05-08 02:17:07 -0400

    import coinhistoryreducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux'
-import { navReducer, coinReducer, coinDetailsReducer } from './reducers'
+import {
+	navReducer,
+	coinReducer,
+	coinDetailsReducer,
+	coinHistoryReducer,
+} from './reducers'
 
 const rootReducer = combineReducers({
 	navbar: navReducer,
 	coinApi: coinReducer,
 	coinDetailsApi: coinDetailsReducer,
+	coinHistoryApi: coinHistoryReducer,
 })
 
 export default rootReducer
