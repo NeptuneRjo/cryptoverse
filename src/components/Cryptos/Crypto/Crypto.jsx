7ec209a98ed52aadbc609b5d2c590cb6c2a5f700
NeptuneRjo <rjo9000@outlook.com>
2022-05-07 21:00:12 -0400

    cleaned up jsx

--- src/components/Cryptos/Crypto/Crypto.jsx
+++ src/components/Cryptos/Crypto/Crypto.jsx
@@ -1,37 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
 
 const Crypto = ({ coinData }) => {
-
-  return (
-    <Link to={`/crypto/${coinData.uuid}`}               data-testid='crypto-item'
-    >
-      <div className="crypto-main" >
-        <div className="crypto-info">
-          <div className="crypto-info-number" data-testid='crypto-num'>
-            {coinData.rank}.
-          </div>
-          <div className="crypto-info-name" data-testid='crypto-name'>
-            {coinData.name}
-          </div>
-          <div className="crypto-info-price" data-testid='crypto-price'>
-            ${Number(coinData.price).toFixed(2)}
-          </div>
-          <p className='crypto-tap'>tap to see more</p>
-          <p className='crypto-click'>click to see more</p>
-        </div>
-        <div className="crypto-icon" >
-          <img 
-            data-testid='crypto-icon'
-            src={coinData.iconUrl} 
-            alt="Cryptocurrency icon" 
-            className="crypto-icon-img"
-          />
-        </div>
-      </div>
-    </ Link>
-  )
+	return (
+		<Link to={`/crypto/${coinData.uuid}`}>
+			<div className='crypto-main'>
+				<div className='crypto-info'>
+					<div className='crypto-info-number'>{coinData.rank}.</div>
+					<div className='crypto-info-name'>{coinData.name}</div>
+					<div className='crypto-info-price'>
+						${Number(coinData.price).toFixed(2)}
+					</div>
+					<p className='crypto-click'>view more...</p>
+				</div>
+				<div className='crypto-icon'>
+					<img
+						src={coinData.iconUrl}
+						alt='Cryptocurrency icon'
+						className='crypto-icon-img'
+					/>
+				</div>
+			</div>
+		</Link>
+	)
 }
 
 export default Crypto
