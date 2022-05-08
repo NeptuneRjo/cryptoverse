02ca23cd127f6f1642df7b479ba57e3b4079706b
NeptuneRjo <rjo9000@outlook.com>
2022-05-08 02:16:54 -0400

    added package for reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 export { default as coinDetailsReducer } from './coinDetailsReducer'
 export { default as coinReducer } from './coinReducer'
 export { default as navReducer } from './navReducer'
+export { default as coinHistoryReducer } from './coinHistoryReducer'
