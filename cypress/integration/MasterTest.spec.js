0e131533fcf7eef5527915d7b1f7735f13c9ff6f
NeptuneRjo <rjo9000@outlook.com>
2022-06-06 19:55:40 -0400

    created master test suite to run all test suites at once

---
+++ cypress/integration/MasterTest.spec.js
@@ -0,0 +1,6 @@
+import CryptoTestSuite from './Cryptos.spec'
+import HomeTestSuite from './Home.spec'
+import NavbarTestSuite from './Navbar.spec'
+
+// Because the test suites call themselves
+// The act of importing them runs the master test suite
